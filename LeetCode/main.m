6160bfcaaba30357334a37a66c8e8bce9b153d94
YOGURTS <scofield838727836@vip.qq.com>
2019-01-25 23:35:00 +0800

    立项

---
+++ LeetCode/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LeetCode
+//
+//  Created by 孙星 on 2019/1/25.
+//  Copyright © 2019 YOGURTS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
